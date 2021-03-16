6c7c6859834bfa65187693e378c8473989f52f1e
Lowe Antony <loweantony.balean@bicol-u.edu.ph>
2021-03-17 06:28:37 +0800

    Add files via upload

---
+++ RegisterSimsCS/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace RegisterSims3CC
+{
+    partial class mainUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainUI));
+            this.Reg = new System.Windows.Forms.Button();
+            this.Unreg = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.gamepath = new System.Windows.Forms.TextBox();
+            this.selfolder = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Reg
+            // 
+            this.Reg.ForeColor = System.Drawing.Color.Black;
+            this.Reg.Location = new System.Drawing.Point(12, 109);
+            this.Reg.Name = "Reg";
+            this.Reg.Size = new System.Drawing.Size(150, 26);
+            this.Reg.TabIndex = 0;
+            this.Reg.Text = "Register The Sims CS";
+            this.Reg.UseVisualStyleBackColor = true;
+            this.Reg.Click += new System.EventHandler(this.Reg_Click);
+            // 
+            // Unreg
+            // 
+            this.Unreg.ForeColor = System.Drawing.Color.Black;
+            this.Unreg.Location = new System.Drawing.Point(165, 109);
+            this.Unreg.Name = "Unreg";
+            this.Unreg.Size = new System.Drawing.Size(150, 26);
+            this.Unreg.TabIndex = 1;
+            this.Unreg.Text = "Remove The Sims CS";
+            this.Unreg.UseVisualStyleBackColor = true;
+            this.Unreg.Click += new System.EventHandler(this.Unreg_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(100, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 32);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Register The Sims\r\nCastaway Stories";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mainUI_MouseDown);
+            this.label2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mainUI_MouseMove);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(9, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(196, 13);
+            this.label5.TabIndex = 29;
+            this.label5.Text = "Select the Sims Castaway Stories folder:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            this.label5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mainUI_MouseDown);
+            this.label5.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mainUI_MouseMove);
+            // 
+            // gamepath
+            // 
+            this.gamepath.ForeColor = System.Drawing.Color.Black;
+            this.gamepath.HideSelection = false;
+            this.gamepath.Location = new System.Drawing.Point(12, 79);
+            this.gamepath.Name = "gamepath";
+            this.gamepath.ReadOnly = true;
+            this.gamepath.Size = new System.Drawing.Size(231, 20);
+            this.gamepath.TabIndex = 30;
+            this.gamepath.TextChanged += new System.EventHandler(this.gamepath_TextChanged);
+            // 
+            // selfolder
+            // 
+            this.selfolder.ForeColor = System.Drawing.Color.Black;
+            this.selfolder.Location = new System.Drawing.Point(249, 76);
+            this.selfolder.Name = "selfolder";
+            this.selfolder.Size = new System.Drawing.Size(66, 26);
+            this.selfolder.TabIndex = 33;
+            this.selfolder.Text = "Browse...";
+            this.selfolder.UseVisualStyleBackColor = true;
+            this.selfolder.Click += new System.EventHandler(this.selfolder_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(69, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(24, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 35;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mainUI_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mainUI_MouseMove);
+            // 
+            // mainUI
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(324, 144);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.selfolder);
+            this.Controls.Add(this.gamepath);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Unreg);
+            this.Controls.Add(this.Reg);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "mainUI";
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Register The Sims CS";
+            this.Load += new System.EventHandler(this.mainUI_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mainUI_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mainUI_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Reg;
+        private System.Windows.Forms.Button Unreg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox gamepath;
+        private System.Windows.Forms.Button selfolder;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ RegisterSimsCS/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace RegisterSims3CC
+{
+
+    public partial class mainUI : Form
+    {
+        public mainUI()
+        {
+            InitializeComponent();
+        }
+        
+        private void Reg_Click(object sender, EventArgs e)
+        {
+            //check if the game folder path is set first.
+            //string gamefolder = gamepath.Text.Replace(@"\", @"\\");
+            string gamefolder = gamepath.Text;
+            if (string.IsNullOrEmpty(gamefolder))
+            {
+                string title = "No Folder Selected";
+                string message = "Please select The Sims Castaway Stories folder.";
+                MessageBox.Show(message, title);
+            }
+            else
+            {
+                /*
+                string message = "The Selected folder is " + gamefolder + ".";
+                MessageBox.Show(message);
+                */
+
+                RegistryKey SimsKey;
+
+                //check the architecture of the OS
+                if (Environment.Is64BitOperatingSystem)
+                {
+                    //check if registered keys exist
+                    if ((Registry.LocalMachine.OpenSubKey("SOFTWARE\\WOW6432Node\\Electronic Arts\\Electronic Arts\\The Sims Castaway Stories")) != null)
+                    {
+                        Registry.LocalMachine.DeleteSubKeyTree("SOFTWARE\\WOW6432Node\\Electronic Arts\\Electronic Arts\\The Sims Castaway Stories");
+                    }
+                    if ((Registry.LocalMachine.OpenSubKey("SOFTWARE\\WOW6432Node\\Electronic Arts\\The Sims Castaway Stories")) != null)
+                    {
+                        Registry.LocalMachine.DeleteSubKeyTree("SOFTWARE\\WOW6432Node\\Electronic Arts\\The Sims Castaway Stories");
+                    }
+                    if ((Registry.LocalMachine.OpenSubKey("SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\App Paths\\SimsCS.exe")) != null)
+                    {
+                        Registry.LocalMachine.DeleteSubKeyTree("SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\App Paths\\SimsCS.exe");
+                    }
+
+                    //register the sims castaway stories
+                    SimsKey = Registry.LocalMachine.CreateSubKey("SOFTWARE\\WOW6432Node\\Electronic Arts");
+                    SimsKey = Registry.LocalMachine.CreateSubKey("SOFTWARE\\WOW6432Node\\Electronic Arts\\Electronic Arts\\The Sims Castaway Stories");
+                    SimsKey = Registry.LocalMachine.CreateSubKey("SOFTWARE\\WOW6432Node\\Electronic Arts\\Electronic Arts\\The Sims Castaway Stories\\egrc");
+                    SimsKey.SetValue("", "N78QRZKW7428DB09DVTY");
+                    SimsKey.Close();
+
+                    SimsKey = Registry.LocalMachine.CreateSubKey("SOFTWARE\\WOW6432Node\\Electronic Arts\\The Sims Castaway Stories");
+                    SimsKey.SetValue("DisplayName", "The Sims™ Castaway Stories");
+                    SimsKey.SetValue("Registration", "SOFTWARE\\WOW6432Node\\Electronic Arts\\Electronic Arts\\The Sims Castaway Stories\\ergc");
+                    SimsKey.SetValue("CacheSize", "3126260736");
+                    SimsKey.SetValue("SwapSize", "0");
+                    SimsKey.SetValue("Language", "English UK");
+                    SimsKey.SetValue("Locale", "en_uk");
+                    SimsKey.SetValue("Install Dir", gamefolder);
+                    SimsKey.SetValue("Product GUID", "{64EEA791-0271-4B53-00AC-2BF05F5FBEF6}");
+                    SimsKey.SetValue("Region", "129397");
+                    SimsKey.SetValue("Suppression Exe", "");
+                    SimsKey.Close();
+
+                    SimsKey = Registry.LocalMachine.CreateSubKey("SOFTWARE\\WOW6432Node\\Electronic Arts\\The Sims Castaway Stories\\1.0");
+                    SimsKey.SetValue("Language", 00000019, RegistryValueKind.DWord);
+                    SimsKey.SetValue("DisplayName", "The Sims™ Castaway Stories");
+                    SimsKey.SetValue("LanguageName", "English UK");
+                    SimsKey.Close();
+                    
+                    SimsKey = Registry.LocalMachine.CreateSubKey("SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\App Paths\\SimsCS.exe");
+                    SimsKey.SetValue("", gamefolder + "\\TSBin\\SimsCS.exe");
+                    SimsKey.SetValue("Path", gamefolder);
+                    SimsKey.SetValue("Game Registry", "SOFTWARE\\WOW6432Node\\Electronic Arts\\The Sims Castaway Stories");
+                    SimsKey.SetValue("Installed", 00000001, RegistryValueKind.DWord);
+                    SimsKey.SetValue("Restart", 00000000, RegistryValueKind.DWord);
+                    SimsKey.SetValue("DirectX Installed", 00000001, RegistryValueKind.DWord);
+
+                    //let's display the registered pack/s via MessageBox
+                    string title = "The Sims CS Registration";
+                    string message = "The Sims Castaway Stories has been registered.";
+                    MessageBox.Show(message, title);
+                }
+                else //else if architecture is 32 bit
+                {
+                    //check if registered keys exist
+                    if ((Registry.LocalMachine.OpenSubKey("SOFTWARE\\Electronic Arts\\Electronic Arts\\The Sims Castaway Stories")) != null)
+                    {
+                        Registry.LocalMachine.DeleteSubKeyTree("SOFTWARE\\Electronic Arts\\Electronic Arts\\The Sims Castaway Stories");
+                    }
+                    if ((Registry.LocalMachine.OpenSubKey("SOFTWARE\\Electronic Arts\\The Sims Castaway Stories")) != null)
+                    {
+                        Registry.LocalMachine.DeleteSubKeyTree("SOFTWARE\\Electronic Arts\\The Sims Castaway Stories");
+                    }
+                    if ((Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\SimsCS.exe")) != null)
+                    {
+                        Registry.LocalMachine.DeleteSubKeyTree("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\SimsCS.exe");
+                    }
+
+                    //register the sims castaway stories
+                    SimsKey = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Electronic Arts");
+                    SimsKey = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Electronic Arts\\Electronic Arts\\The Sims Castaway Stories");
+                    SimsKey = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Electronic Arts\\Electronic Arts\\The Sims Castaway Stories\\egrc");
+                    SimsKey.SetValue("", "N78QRZKW7428DB09DVTY");
+                    SimsKey.Close();
+
+                    SimsKey = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Electronic Arts\\The Sims Castaway Stories");
+                    SimsKey.SetValue("DisplayName", "The Sims™ Castaway Stories");
+                    SimsKey.SetValue("Registration", "SOFTWARE\\Electronic Arts\\Electronic Arts\\The Sims Castaway Stories\\ergc");
+                    SimsKey.SetValue("CacheSize", "3126260736");
+                    SimsKey.SetValue("SwapSize", "0");
+                    SimsKey.SetValue("Language", "English UK");
+                    SimsKey.SetValue("Locale", "en_uk");
+                    SimsKey.SetValue("Install Dir", gamefolder);
+                    SimsKey.SetValue("Product GUID", "{64EEA791-0271-4B53-00AC-2BF05F5FBEF6}");
+                    SimsKey.SetValue("Region", "129397");
+                    SimsKey.SetValue("Suppression Exe", "");
+                    SimsKey.Close();
+
+                    SimsKey = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Electronic Arts\\The Sims Castaway Stories\\1.0");
+                    SimsKey.SetValue("Language", 00000019, RegistryValueKind.DWord);
+                    SimsKey.SetValue("DisplayName", "The Sims™ Castaway Stories");
+                    SimsKey.SetValue("LanguageName", "English UK");
+                    SimsKey.Close();
+
+                    SimsKey = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\SimsCS.exe");
+                    SimsKey.SetValue("", gamefolder + "\\TSBin\\SimsCS.exe");
+                    SimsKey.SetValue("Path", gamefolder);
+                    SimsKey.SetValue("Game Registry", "SOFTWARE\\Electronic Arts\\The Sims Castaway Stories");
+                    SimsKey.SetValue("Installed", 00000001, RegistryValueKind.DWord);
+                    SimsKey.SetValue("Restart", 00000000, RegistryValueKind.DWord);
+                    SimsKey.SetValue("DirectX Installed", 00000001, RegistryValueKind.DWord);
+
+                    //let's display the registered pack/s via MessageBox
+                    string title = "The Sims CS Registration";
+                    string message = "The Sims Castaway Stories has been registered.";
+                    MessageBox.Show(message, title);
+                }
+                
+            }
+        }
+        
+        private void gamepath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selfolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog selectpath = new FolderBrowserDialog();
+            selectpath.Description = "Select the game folder of the Sims Castaway Stories:";
+
+            if (selectpath.ShowDialog() == DialogResult.OK)
+            {
+                gamepath.Text = selectpath.SelectedPath;
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Unreg_Click(object sender, EventArgs e)
+        {
+            DialogResult confirmUnregister = MessageBox.Show("This will remove the registration of The Sims Castaway Stories in this computer. Are you sure you want to proceed?", "Remove The Sims CS", MessageBoxButtons.YesNo);
+            if (confirmUnregister == DialogResult.Yes)
+            {
+                //check the architecture of the OS
+                if (Environment.Is64BitOperatingSystem)
+                {
+                    if ((Registry.LocalMachine.OpenSubKey("SOFTWARE\\WOW6432Node\\Electronic Arts\\Electronic Arts\\The Sims Castaway Stories")) != null)
+                    {
+                        Registry.LocalMachine.DeleteSubKeyTree("SOFTWARE\\WOW6432Node\\Electronic Arts\\Electronic Arts\\The Sims Castaway Stories");
+                    }
+                    if ((Registry.LocalMachine.OpenSubKey("SOFTWARE\\WOW6432Node\\Electronic Arts\\The Sims Castaway Stories")) != null)
+                    {
+                        Registry.LocalMachine.DeleteSubKeyTree("SOFTWARE\\WOW6432Node\\Electronic Arts\\The Sims Castaway Stories");
+                    }
+                    if ((Registry.LocalMachine.OpenSubKey("SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\App Paths\\SimsCS.exe")) != null)
+                    {
+                        Registry.LocalMachine.DeleteSubKeyTree("SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\App Paths\\SimsCS.exe");
+                    }
+                    
+                    string title = "Remove The Sims CS";
+                    string message = "The Sims Castaway Stories has been removed.";
+                    MessageBox.Show(message, title);
+                }
+                else //else if architecture is 32bit
+                {
+                    if ((Registry.LocalMachine.OpenSubKey("SOFTWARE\\Electronic Arts\\Electronic Arts\\The Sims Castaway Stories")) != null)
+                    {
+                        Registry.LocalMachine.DeleteSubKeyTree("SOFTWARE\\Electronic Arts\\Electronic Arts\\The Sims Castaway Stories");
+                    }
+                    if ((Registry.LocalMachine.OpenSubKey("SOFTWARE\\Electronic Arts\\The Sims Castaway Stories")) != null)
+                    {
+                        Registry.LocalMachine.DeleteSubKeyTree("SOFTWARE\\Electronic Arts\\The Sims Castaway Stories");
+                    }
+                    if ((Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\SimsCS.exe")) != null)
+                    {
+                        Registry.LocalMachine.DeleteSubKeyTree("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\SimsCS.exe");
+                    }
+
+                    string title = "Remove The Sims CS";
+                    string message = "The Sims Castaway Stories has been removed.";
+                    MessageBox.Show(message, title);
+                }
+            }
+        }
+        
+        private void mainUI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //this enables draging the window somewhere in the screen
+        private void mainUI_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left || e.Button == MouseButtons.Right)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+        Point lastPoint;
+        private void mainUI_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+    }
+}
